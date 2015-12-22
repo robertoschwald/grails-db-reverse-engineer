204b6f717073828336e133db27488429d21b2dc6
Burt Beckwith <burt@burtbeckwith.com>
2015-12-22 15:55:25 -0500

    updated to hibernate-tools 3.6.0.Final, cleaned up dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,5 +1,5 @@
 grails.project.work.dir = 'target'
-grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
+grails.project.docs.output.dir = 'target.docs/manual'
 
 grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
@@ -14,32 +14,11 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		compile('org.hibernate:hibernate-tools:3.6.0.CR1') {
-			excludes 'ant', 'ant-launcher', 'cglib', 'common', 'commons-logging', 'freemarker',
-			         'hibernate-core', 'hibernate-entitymanager', 'hibernate-jpa-2.0-api', 'hsqldb',
-			         'jaxen', 'javassist', 'jta', 'jtidy', 'junit', 'org.eclipse.jdt.core', 'runtime',
-			         'slf4j-log4j12', 'text'
+		compile 'org.hibernate:hibernate-tools:3.6.0.Final', {
+			excludes 'ant', 'cglib', 'common', 'org.eclipse.jdt.core', 'runtime', 'text'
 		}
 
-		compile('org.hibernate:hibernate-core:3.6.10.Final') {
-			excludes 'ant', 'antlr', 'cglib', 'commons-collections', 'commons-logging', 'commons-logging-api',
-			         'dom4j', 'h2', 'hibernate-commons-annotations', 'hibernate-jpa-2.0-api',
-			         'hibernate-validator', 'javassist', 'jaxb-api', 'jaxb-impl', 'jboss-jacc-api_JDK4',
-			         'jcl-over-slf4j', 'jta', 'junit', 'slf4j-api', 'slf4j-log4j12', 'validation-api'
-		}
-
-		compile('dom4j:dom4j:1.6.1') {
-			excludes 'jaxen', 'jaxme-api', 'junitperf', 'pull-parser', 'relaxngDatatype',
-			         'stax-api', 'stax-ri', 'xalan', 'xercesImpl', 'xml-apis', 'xpp3', 'xsdlib'
-		}
-
-		compile('org.hibernate:hibernate-commons-annotations:3.2.0.Final') {
-			excludes 'commons-logging', 'commons-logging-api', 'jcl-over-slf4j', 'junit', 'slf4j-api', 'slf4j-log4j12'
-		}
-
-		compile 'freemarker:freemarker:2.3.8'
-
-		compile 'org.hibernate:jtidy:r8-20060801'
+		compile 'org.hibernate:hibernate-core:3.6.10.Final'
 	}
 
 	plugins {
