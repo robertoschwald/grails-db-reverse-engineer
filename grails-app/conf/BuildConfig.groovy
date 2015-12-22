7b4ddabdd96ad0a9292d775133223e2d7a846a68
Burt Beckwith <burt@burtbeckwith.com>
2015-12-22 14:44:54 -0500

    update to Grails 2.4.5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,14 +1,15 @@
 grails.project.work.dir = 'target'
 grails.project.docs.output.dir = 'docs/manual' // for backwards-compatibility, the docs are checked into gh-pages branch
 
+grails.project.dependency.resolver = 'maven'
 grails.project.dependency.resolution = {
 
 	inherits 'global'
 	log 'warn'
 
 	repositories {
-		grailsCentral()
 		mavenLocal()
+		grailsCentral()
 		mavenCentral()
 	}
 
@@ -42,7 +43,7 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+		build ':release:3.1.2', ':rest-client-builder:2.1.1', {
 			export = false
 		}
 	}
