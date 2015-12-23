461ba2f208c6b54212513ab48dc4bf1a10259ad1
Burt Beckwith <burt@burtbeckwith.com>
2015-12-23 14:28:11 -0500

    docs for the 3.0.0 release

--- release.sh
+++
@@ -1,8 +0,0 @@
-rm -rf target/release
-mkdir target/release
-cd target/release
-git clone git@github.com:grails-plugins/grails-db-reverse-engineer.git
-cd grails-db-reverse-engineer
-grails clean
-grails compile
-grails publish-plugin --stacktrace
